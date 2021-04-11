7d1d3450e6ca9648d6cb17cd6524227006ab0e81
=itspedrohrs <pedro.silva4@ftc.edu.br>
2021-04-11 19:33:04 -0300

    create all routes in categories and products

--- routes/api.php
+++ routes/api.php
@@ -3,17 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::get('products', 'ProductController@index');
+Route::post('products', 'ProductController@create');
+Route::get('products/{id}', 'ProductController@index');
+Route::put('products/{id}', 'ProductController@update');
+Route::delete('products/{id}', 'ProductController@delete');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::get('categories', 'CategoryController@index');
+Route::post('categories', 'CategoryController@create');
+Route::get('categories/{id}', 'CategoryController@index');
+Route::put('categories/{id}', 'CategoryController@update');
+Route::delete('categories/{id}', 'CategoryController@delete');
