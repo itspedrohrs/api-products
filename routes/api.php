93067cf5bdbf83f0e882d96a3983f7e4c3128d87
=itspedrohrs <pedro.silva4@ftc.edu.br>
2021-04-11 19:40:34 -0300

    create all routes in categories and products

--- routes/api.php
+++ routes/api.php
@@ -5,12 +5,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('products', 'ProductController@index');
 Route::post('products', 'ProductController@create');
-Route::get('products/{id}', 'ProductController@index');
+Route::get('products/{id}', 'ProductController@show');
 Route::put('products/{id}', 'ProductController@update');
 Route::delete('products/{id}', 'ProductController@delete');
 
 Route::get('categories', 'CategoryController@index');
-Route::post('categories', 'CategoryController@create');
-Route::get('categories/{id}', 'CategoryController@index');
+Route::post('categories', 'CategoryController@store');
+Route::get('categories/{id}', 'CategoryController@show');
 Route::put('categories/{id}', 'CategoryController@update');
 Route::delete('categories/{id}', 'CategoryController@delete');
