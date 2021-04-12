2a05599bcf72077428b9b6afaabd3675493b3f55
=itspedrohrs <pedro.silva4@ftc.edu.br>
2021-04-11 21:04:55 -0300

    Alter routes path api to controllers

--- routes/api.php
+++ routes/api.php
@@ -3,14 +3,14 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('products', 'ProductController@index');
-Route::post('products', 'ProductController@create');
-Route::get('products/{id}', 'ProductController@show');
-Route::put('products/{id}', 'ProductController@update');
-Route::delete('products/{id}', 'ProductController@delete');
+Route::get('products', 'api\\ProductController@index');
+Route::post('products', 'api\\ProductController@create');
+Route::get('products/{id}', 'api\\ProductController@show');
+Route::put('products/{id}', 'api\\ProductController@update');
+Route::delete('products/{id}', 'api\\ProductController@delete');
 
-Route::get('categories', 'CategoryController@index');
-Route::post('categories', 'CategoryController@store');
-Route::get('categories/{id}', 'CategoryController@show');
-Route::put('categories/{id}', 'CategoryController@update');
-Route::delete('categories/{id}', 'CategoryController@delete');
+Route::get('categories', 'api\\CategoryController@index');
+Route::post('categories', 'api\\CategoryController@store');
+Route::get('categories/{id}', 'api\\CategoryController@show');
+Route::put('categories/{id}', 'api\\CategoryController@update');
+Route::delete('categories/{id}', 'api\\CategoryController@delete');
